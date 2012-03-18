c3de236354024949adf486e0d51145885d493af7
Notdeadyetyouknow <Notdeadyetyouknow@localhost>
2012-03-18 13:33:36 +0200

    ABC... 1234 ruudu klikkamine

--- gamefield.js
+++ gamefield.js
@@ -4,12 +4,14 @@ window.onload = function(){
 $(document).ready(function() {
 	$("#playField td").click(function(event){
 		td = event.target;
-		if ($(td).css("background-color")=="rgb(255, 255, 255)"){
-		    $(td).css("background-color", "")
+		if (td.cellIndex!=0 & td.parentElement.rowIndex!=0){
+			if ($(td).css("background-color")=="rgb(255, 255, 255)"){
+				$(td).css("background-color", "")
+			}
+			else {
+				$(td).css("background-color", "#FFFFFF")
+			}
+			//console.log(td.cellIndex +","+ td.parentElement.rowIndex)
 		}
-		else {
-		    $(td).css("background-color", "#FFFFFF")
-		}
-		//console.log(td.cellIndex +","+ td.parentElement.rowIndex)
-		});
+	});
 });
