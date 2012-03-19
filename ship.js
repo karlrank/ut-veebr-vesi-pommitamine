264959e3eba56ea1cf51e00d69f7b95950a0ebc5
karlrank <karlrank@localhost>
2012-03-19 22:59:43 +0200

    VALMIS

--- ship.js
+++ ship.js
@@ -2,11 +2,9 @@ function Ship(coordinates) {
 	this.coordinates = coordinates;
 	if (this.coordinates.length == 1) {
 		this.orientation = 0; //one square ship, orientation irrelavent
-	}
-	else if (this.coordinates[0][0] == this.coordinates[1][0]) {
+	} else if (this.coordinates[0][0] == this.coordinates[1][0]) {
 		this.orientation = 1; //horizontal
-	}
-	else if (this.coordinates[0][1] == this.coordinates[1][1]) {
+	} else if (this.coordinates[0][1] == this.coordinates[1][1]) {
 		this.orientation = 2; //vertical
 	}
 	this.length = coordinates.length;
